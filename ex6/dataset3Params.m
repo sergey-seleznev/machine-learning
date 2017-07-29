d2c97245cd7b1f0169da5a78a45a48046fc28726
Sergey Seleznev <sergey@prfrmr.tech>
2017-07-30 00:23:24 +0300

    Solve exercise #6

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -7,28 +7,26 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
-
-
-
-
-
-
-
-% =========================================================================
+% DETERMINING OPTIMAL VALUES:
+% options = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30 ];
+% optionCount = length(options);
+% minErr = 10^9;
+% for i = 1:optionCount
+%   currentC = options(i);
+%   for j = 1:optionCount
+%     currentSigma = options(j);
+%     model = svmTrain(X, y, currentC, @(x1, x2) gaussianKernel(x1, x2, currentSigma));
+%     predictions = svmPredict(model, Xval);
+%     err = mean(double(predictions ~= yval));
+%     if (err < minErr)
+%       minErr = err;
+%       C = currentC;
+%       sigma = currentSigma;
+%     end
+%   end
+% end
 
 end
